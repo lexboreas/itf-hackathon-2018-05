425dd1b2e9f483eefbc17bdeb64e56b6b7d6f1f9
Tom Counsell <other@tomcounsell.com>
2018-06-01 11:29:35 +0700

    bbands example

--- girlslikemyta/handler.py
+++ girlslikemyta/handler.py
@@ -2,9 +2,16 @@ import json
 
 
 def test_ta_lib(event, context):
+    import numpy
+    import talib
+
+    close = numpy.random.random(100)
+    output = talib.SMA(close)
+
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
+        "input": event,
+        "last_sma": output[-1],
     }
 
     response = {
@@ -22,3 +29,28 @@ def test_ta_lib(event, context):
         "event": event
     }
     """
+
+
+def calcBollingerBands(event, context):
+    import numpy as np
+    # note that all ndarrays must be the same length!
+    inputs = {
+        'open': np.random.random(100),
+        'high': np.random.random(100),
+        'low': np.random.random(100),
+        'close': np.random.random(100),
+        'volume': np.random.random(100)
+    }
+
+    from talib.abstract import BBANDS
+
+    upper, middle, lower = BBANDS(inputs, 20, 2, 2)
+
+    return {
+        "statusCode": 200,
+        "BBANDS": json.dumps({
+            'upper': list(upper),
+            'middle': list(middle),
+            'lower': list(lower)
+        })
+    }
