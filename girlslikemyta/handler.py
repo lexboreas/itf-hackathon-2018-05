c46647dafbd1a36771765e79cd8d41ef327b1d85
Tom Counsell <other@tomcounsell.com>
2018-06-02 12:43:12 +0700

    edit dynamo queries

--- girlslikemyta/handler.py
+++ girlslikemyta/handler.py
@@ -3,29 +3,57 @@ import json
 import boto3
 
 
+def test_sns(event, context):
+    print(event)
+    return {"statusCode": 200} #ok
+
+# {'Records': [{
+#     'EventSource': 'aws:sns',
+#     'EventVersion': '1.0',
+#     'EventSubscriptionArn': 'arn:aws:sns:us-east-1:983584755688:itt-sns-data-core-stage:493ba9a2-52f0-4c12-b1a4-7bdaee803690'
+#         ,
+#     'Sns': {
+#         'Type': 'Notification',
+#         'MessageId': 'dc693f13-b8d8-5445-88b1-6c9954471f92',
+#         'TopicArn': 'arn:aws:sns:us-east-1:983584755688:itt-sns-data-core-stage'
+#             ,
+#         'Subject': None,
+#         'Message': '[{"source": "binance", "category": "price", "symbol": "ETH/BTC", "value": 0.077133, "timestamp": 1527835230.836},
+#
+
 def test_dynamodb(event, context):
     from boto3.dynamodb.conditions import Key, Attr
     dynamodb = boto3.resource('dynamodb')
     indicators_table = dynamodb.Table(os.environ['DYNAMODB_INDICATORS_TABLE'])
+    #
+    # for record in event['Records']['Sns']['Message']
+    #     if record['category'] == "price":
+    #         db_response = price_table.put_item(
+    #             Item = {
+    #                 'primary_key': "%s_%s_%d" % (data['source'], data['ticker'], data['unix_timestamp']),
+    #                 'source': record['source'],
+    #                 'ticker': record['symbol'],
+    #                 'price': record['value'],
+    #                 'unix_timestamp': int(parseFloat(record['timestamp']))
+    #             }
+    #         )
 
-    try:
-        db_response = indicators_table.get_item(
-            Key={'primary_key': "special-string-key"}
-        )
+    db_response = indicators_table.get_item(
+        Key={'primary_key': "special-string-key2"}
+    )
+    if "Item" in db_response:
         message = "it was already there"
-    except:
+    else: #create item in DynamoDB table
         db_response = indicators_table.put_item(
             Item = {
-                'primary_key': "special-string-key",
+                'primary_key': "special-string-key2",
                 'unix_timestamp': 1527760162,
-                'created_at': str(int(time.mktime((datetime.now()).timetuple()))),
-                'modified_at': str(int(time.mktime((datetime.now()).timetuple()))),
             }
         )
-        message = "pretty sure we saved in the db"
+        message = "we saved in the db"
 
     body = {"message": message,
-            "db_response": json.dumps(db_response)
+            "db_response": str(db_response)
             }
 
     response = {
